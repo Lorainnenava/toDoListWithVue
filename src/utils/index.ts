d09e0c732d4b2764d53ee1518af291a282c17f72
LorainneNavarro <lnavarro@wowdesarrollos.com>
2024-08-21 17:02:02 -0500

    cambiando longitud del codigo

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -34,7 +34,7 @@ export const CodeRandom = () => {
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let randomCode = "";
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 5; i++) {
     randomCode += characters.charAt(
       Math.floor(Math.random() * characters.length)
     );
